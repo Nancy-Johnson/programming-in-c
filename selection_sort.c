f30d5e7021b4eae3fb228dafaafa4ef91253857a
Nancy-Johnson <96773441+Nancy-Johnson@users.noreply.github.com>
2022-04-07 23:11:24 +0530

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+   int arr[10]={10,2,7,22,90,3,3,0,5,11};
+   int n=10;
+   int i, j, position, swap;
+   for (i = 0; i < (n - 1); i++) {
+      position = i;
+      for (j = i + 1; j < n; j++) {
+         if (arr[position] > arr[j])
+            position = j;
+      }
+      if (position != i) {
+         swap = arr[i];
+         arr[i] = arr[position];
+         arr[position] = swap;
+      }
+   }
+   for (i = 0; i < n; i++)
+      printf("%d\t", arr[i]);
+   return 0;
+}
